5ed15310fd5ec6a3dfcdc75a6d7e63c32224dc05
Mihajlo Pavlovic <mihajlo.pavlovic@origin-trail.com>
2024-05-25 15:48:56 +0800

    add

--- plonk-activity/plonk/sample_problem.py
+++ plonk-activity/plonk/sample_problem.py
@@ -1,11 +1,15 @@
 from .copy_constraint import find_permutation
 from .constraint import add_mul_constarint, add_add_constarint, add_constant_constraint
 
+def gen_witness():
+    # Prove that we know (x, y, z) such that x^2 + y^2 = z^2
+    x = 3
+    y = 4
+    z = 5
 
-def gen_witness(x):
-    a = [x, x * x, x * x * x, 1, 1, x * x * x + x]
-    b = [x, x, x, 5, 35, 5]
-    c = [x * x, x * x * x, x + x * x * x, 5, 35, 35]
+    a = [x, y, x*x, y*y, z*z, x*x + y*y]
+    b = [x, y, 1, 1, 1, 1]
+    c = [x*x, y*y, x*x, y*y, z*z, z*z]
 
     return (a, b, c)
 
@@ -13,14 +17,14 @@ def gen_witness(x):
 def is_satisfied_witness(a, b, c):
     assert a[0] * b[0] == c[0]
     assert a[1] * b[1] == c[1]
-    assert a[2] + b[2] == c[2]
+    assert a[2] * b[2] == c[2]
     assert a[3] * b[3] == c[3]
     assert a[4] * b[4] == c[4]
-    assert a[5] + b[5] == c[5]
+    assert a[5] == c[5]
 
 
 def gen_constraints():
-    # Prove that I know an X such that X*x*x + x +5 == 35
+    # Prove that we know (x, y, z) such that x^2 + y^2 = z^2
 
     # init constraints
     Ql = []
@@ -30,26 +34,26 @@ def gen_constraints():
     Qc = []
 
     # set constraints
-    Ql, Qr, Qm, Qo, Qc = add_mul_constarint(Ql, Qr, Qm, Qo, Qc)
-    Ql, Qr, Qm, Qo, Qc = add_mul_constarint(Ql, Qr, Qm, Qo, Qc)
-    Ql, Qr, Qm, Qo, Qc = add_add_constarint(Ql, Qr, Qm, Qo, Qc)
-    Ql, Qr, Qm, Qo, Qc = add_constant_constraint(Ql, Qr, Qm, Qo, Qc, 5)
-    Ql, Qr, Qm, Qo, Qc = add_constant_constraint(Ql, Qr, Qm, Qo, Qc, 35)
-    # todo add a constant constraint for 1
-    Ql, Qr, Qm, Qo, Qc = add_add_constarint(Ql, Qr, Qm, Qo, Qc)
+    Ql, Qr, Qm, Qo, Qc = add_mul_constarint(Ql, Qr, Qm, Qo, Qc)  # x * x = x^2
+    Ql, Qr, Qm, Qo, Qc = add_mul_constarint(Ql, Qr, Qm, Qo, Qc)  # y * y = y^2
+    Ql, Qr, Qm, Qo, Qc = add_constant_constraint(Ql, Qr, Qm, Qo, Qc, 1)  # 1
+    Ql, Qr, Qm, Qo, Qc = add_constant_constraint(Ql, Qr, Qm, Qo, Qc, 1)  # 1
+    Ql, Qr, Qm, Qo, Qc = add_constant_constraint(Ql, Qr, Qm, Qo, Qc, 1)  # 1
+    Ql, Qr, Qm, Qo, Qc = add_add_constarint(Ql, Qr, Qm, Qo, Qc)  # x^2 + y^2 = z^2
+
     return (Ql, Qr, Qm, Qo, Qc)
 
 
 def gen_copy_constraints():
     # copy constraints
-    # a = [x , x*x, x*x*x, 1,  1, x*x*x + x]
-    # b = [x , x, x, 5, 35, 5]
-    # c = [x*x, x*x*x , x + x*x*x ,5, 35, 35]
-    # inputs  = [x , x*x, x*x*x, 1,  1,
-    #           x*x*x + x, x , x, x, 5, 35, 5
-    #           x*x, x*x*x , x + x*x*x ,5, 35, 35]
+    # a = [x, y, x*x, y*y, z*z, x*x + y*y]
+    # b = [x, y, 1, 1, 1, 1]
+    # c = [x*x, y*y, x*x, y*y, z*z, z*z]
+    # inputs = [x, y, x*x, y*y, z*z, x*x + y*y,
+    #           x, y, 1, 1, 1, 1,
+    #           x*x, y*y, x*x, y*y, z*z, z*z]
 
-    copy_constraints = [8, 12, 13, 3, 4, 14, 0, 6, 7, 15, 17, 9, 1, 2, 5, 11, 10, 16]
+    copy_constraints = [0, 6, 12, 1, 7, 13, 2, 8, 14, 3, 9, 15, 4, 10, 16, 5, 11, 17]
 
     eval_domain = range(0, len(copy_constraints))
 
